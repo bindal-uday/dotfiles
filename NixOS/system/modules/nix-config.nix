7f1feb2105f1dfd7cf45ffd453bfeee61fc9250a
ERROR <bindaluday23.12.2004@gmail.com>
2025-03-15 17:24:39 +0530

    test commit 2

--- NixOS/system/modules/nix-config.nix
+++ NixOS/system/modules/nix-config.nix
@@ -16,15 +16,15 @@
       auto-optimise-store = true
     '';
   
-    # settings = {
-    #   trusted-users = [ "root" "levi" "@wheel" ];
-    #   substituters = [
-    #     "https://hyprland.cachix.org"
-    #   ];
-    #   trusted-public-keys = [
-    #     "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    #   ];
-    # };
+    settings = {
+      trusted-users = [ "root" "levi" "@wheel" ];
+      substituters = [
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+    };
 
     # auto scheduled GC running
     gc = {
